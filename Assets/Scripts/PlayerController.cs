ab04810d5ab3a623f1858831c2c1a5e55cb7aece
Sebastian Baxter <sebastian.baxter.personal@gmail.com>
2025-02-21 10:53:26 -0500

    Programming a simple Camera Controller
    gy

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         {
+        
+        // Align to player position
+        transform.position = player.transform.position;
+        
+        // Rotate left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * 100, Space.World);
+        }
+        
+        // Rotate right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 100, Space.World);
+        }
+    }
+    }
+}
